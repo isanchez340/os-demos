f6e109cc2709bacfc30b19081970ce202a25f2cc
Irving <sanchez.irving006@gmail.com>
2018-09-10 20:50:43 -0600

    added user input

--- ch5-api/p3-exec.py
+++ ch5-api/p3-exec.py
@@ -3,7 +3,6 @@
 import os, sys, time, re
 
 pid = os.getpid()
-
 os.write(1, ("About to fork (pid:%d)\n" % pid).encode())
 
 rc = os.fork()
@@ -13,9 +12,10 @@ if rc < 0:
     sys.exit(1)
 
 elif rc == 0:                   # child
-    os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % 
+    Uinput=input("$")
+    os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" %
                  (os.getpid(), pid)).encode())
-    args = ["wc", "p3-exec.py"]
+    args = ["wc", Uinput]
     for dir in re.split(":", os.environ['PATH']): # try each directory in the path
         program = "%s/%s" % (dir, args[0])
         os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
@@ -28,8 +28,8 @@ elif rc == 0:                   # child
     sys.exit(1)                 # terminate with error
 
 else:                           # parent (forked ok)
-    os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" % 
+    os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" %
                  (pid, rc)).encode())
     childPidCode = os.wait()
-    os.write(1, ("Parent: Child %d terminated with exit code %d\n" % 
+    os.write(1, ("Parent: Child %d terminated with exit code %d\n" %
                  childPidCode).encode())
